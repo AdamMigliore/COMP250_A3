7fbd6ccde6da9c351d764f0422fe1f644f2559b3
AdamMigliore <adam.dire@gmail.com>
2020-03-10 20:30:05 -0400

    first commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n========================\n" + "| Welcome to Cat Cafe! |\n" + "========================\n");
+		
+		createExampleCats();
+		
+	}
+	
+	//Creates all the cats from the initial example of addcats
+	private static void createExampleCats() {};
+
+}
